4d1111d061f9a29f2a90282f35351207a710579f
Tomas Woo <tommy@happygrunt.com>
2013-06-22 10:02:03 -0700

    just doing an update

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from functools import wraps
 from flask import Flask, jsonify, request, json, Response
 
-# from flask.ext.pymongo import PyMongo
+from flask.ext.pymongo import PyMongo
+from flask.ext.mail import Mail, Message
+# from pymongo import Connection
 
 from datetime import *
 import time
 
 app = Flask(__name__)
 
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_DBNAME'] = 'rsvp'
+mongo = PyMongo(app)
+# c = Connection('localhost')
+
+mail=Mail(app)
+
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'tommy.angico@gmail.com',
+	MAIL_PASSWORD = 'healinghope3'
+	)
+mail=Mail(app)
 
 def support_jsonp(f):
     """Wraps JSONified output for JSONP"""
@@ -18,47 +37,60 @@ def support_jsonp(f):
         if callback:
             content = str(callback) + '(' + str(f().data) + ')'
             return app.response_class(content,
-                    mimetype='application/json')
+                    mimetype='application/javascript')
         else:
             return f(*args, **kwargs)
     return decorated_function
 
-# app.debug = True
-# app.config['SECRET_KEY'] = '\x1b\x02\xe5\xce\xcd\xc3\x85\x14\xd1=\x04\xddA*E\xe0\xd7\xac\xc35\x10w\x98\x8d'
+app.config['SECRET_KEY'] = 'ILOVEYOUangela'
 # mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
-    return 'hello my love Angela'
+    return 'hello my love, Angela'
 
 
 @app.route('/countdown/')
 @app.route('/countdown/<yyyymmdd>', methods=['GET'])
 @support_jsonp
 def show_countdown_days(yyyymmdd=None):
-
-# curl -i -H "Content-type: application/json" -X GET -d {"rsvp_email":"tommy@hotmail.com"}' http://localhost:9999/countdown/
-    # date_requested = datetime.strptime(yyyymmdd,'%Y%m%d').date()
-
+# curl -i -H "Content-type: application/json" -X POST -d 'firstName=Tommy&lastName=Wu&attending=true' http://localhost:5000/rsvp/
     if request.mimetype == 'application/json':
         if yyyymmdd == None:
             return jsonify({'status': '200', 'daysleft': days_until()})
     else:
-
         # print datetime.datetime.strptime(yyyymmdd,'%Y%m%d').date()
-
         if yyyymmdd == None:
-            return jsonify({'daysleft': days_until()})
+            return jsonify({'dayslefts': days_until()})
 
+
+@app.route('/rsvp/', methods=['GET','POST'])
+@support_jsonp
+def rsvp():
+	#curl -i -H "Content-tjson" -X GET -d {"firstName":"Tommy", "lastName":"Wu","attending":true} http://localhost:5000/rsvp/
+	if request.mimetype == 'application/json' and request.method == 'GET':
+		return request.data+'\n'
+
+	guest = mongo.db.guest.find_one({'firstName':'Tommy'})
+	print guest
+	return str(guest)
+
+@app.route('/email/')
+@support_jsonp
+def send_email():
+	# b1e78df7be8c36337dd3197d03b16d10
+	if request.method == 'GET':
+		# msg = Message('Hello there', sender=("Angela & Tommy", "tommg.angico@gmail.com"), recipients=['wu.thomas@gmail.com'])
+		# msg.body = "This is the email body"
+		# mail.send(msg)
+		print request['headers']+'..................'
+		return jsonify(msg='it works')
+	print request.mimetype
 
 def days_until():
     weddingDay = date(2013, 9, 15)
     diff = weddingDay - date.today()
     return abs(diff.days)
-
-
-
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
