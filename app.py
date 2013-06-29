dd61262331fb41ef25db31d65a5264ccfb2cbdb0
Tomas Woo <tommy@happygrunt.com>
2013-06-29 13:49:31 -0700

    major updates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from ast import literal_eval
 from flask import Flask, jsonify, request, json, Response, Request, render_template
 from pymongo import MongoClient
 
@@ -69,10 +70,58 @@ def show_countdown_days(yyyymmdd=None):
 @support_jsonp
 def rsvp():
     a = []
+    sender='tommy.angico@gmail.com'
+    host_email = ''
     # print request.args
-
+# {u'email': u'cord.of.three@gmail.com', u'firstname': u'Angela', u'isAttending': True, u'lastname': u'Hsieh', u'timeStamp': datetime.datetime(2013, 6, 27, 16, 1, 58, 534000), u'guestOf': u'', u'_id': ObjectId('51ccbbedb181d7b90ccbf74c')}
     if request.args:
-        return request.args
+        alist = []
+        
+        # catches just the first host
+        if request.args.get('email_of_host'):
+            aperson = {'email':request.args.get('email_of_host'), 'guestOf':''}
+            aperson['firstname'] = request.args.get('firstname')
+            aperson['timeStamp'] = datetime.now()
+            aperson['lastname'] = request.args.get('lastname')
+            aperson['isAttending'] = request.args.get('is_attending')
+            print '\n'
+            print aperson
+            print '\n'
+            obj_id = collection.insert(aperson)
+            
+            host_email = aperson['email']
+            
+            if obj_id and aperson['isAttending'] == 'true':
+                msg = Message('Thank you for RSVPing', sender = 'tommy.angico@gmail.com', recipients=[host_email])
+                msg.body = "So happy that you are coming! \n A few details to remind you: \n\nCeremony @ 3pm - Messiah Lutheran Church - 4861 Liverpool St Yorba Linda, CA 92886 \n\n Cocktail hour and reception @5pm - Summit House - 2000 E Bastanchury Rd, Fullerton, CA 92835 \n\n\n Please feel free to email us if you have any questions! Check back at wedding.happygrunt.com for updates \n\n\n Looking forward to celebrating with you!"       
+                mail.send(msg)
+            
+
+        # When there is a guest list
+        if request.args.get('guestlist'):
+            guestlist = request.args.get('guestlist')
+            
+            for aguest in guestlist.split('|'):
+                bperson = {'email':'', 'guestOf':request.args.get('guest_of'), 'isAttending':'true'}
+                bperson['firstname'] = aguest.split(',')[0].split('firstname=')[1]
+                bperson['timeStamp'] = datetime.now()
+                bperson['lastname'] = aguest.split(',')[1].split('lastname=')[1]
+                alist.append(bperson)
+                
+            print '\n\ninserting this list of objects\n\n', alist , '\n\n\n'
+            # print guestlist.split('|')[0]
+            # print literal_eval(guestlist.split('|')[0])
+            collection.insert(alist)
+
+        if host_email and len(alist) != 0:
+            names = ''
+            for guest in alist:
+                names += guest['firstname']+' '+guest['lastname']+' '
+            msg = Message('RSVP - Guest', sender = 'tommy.angico@gmail.com', recipients=[host_email])
+            msg.body = "These people are coming as your guests: "+ names
+            mail.send(msg)
+        
+        return jsonify(firstname='test')
     # print request.headers['Accept']
     for guest in collection.find():
         a.append(guest)
@@ -81,6 +130,16 @@ def rsvp():
     # return str(collection)
 
 #curl -i -H "Content-tjson" -X GET -d {"firstName":"Tommy", "lastName":"Wu","attending":true} http://localhost:5000/rsvp/
+@app.route('/edit/')
+@app.route('/edit/<int:obj_id>')
+def edit_doc(obj_id):
+    return obj_id
+
+@app.route('/killdata/')
+def killall():
+    test = collection.remove()
+    print test
+    return 'data killed'
 
 @app.route('/email/')
 @support_jsonp
